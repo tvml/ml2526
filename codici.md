b82dc1b5bf1ee7356dd77e48b9cc86ba642f6ed2
tvml <gambosi@gmail.com>
2018-03-25 19:23:32 +0200

    250318

--- codici.md
+++ codici.md
@@ -7,7 +7,7 @@ layout: default
 1. [`iris.`{:.codelink}]({{ site.baseurl }}{%link codici/iris.py %}) Visualizzazione del dataset Iris (script)
 1. [`linregrsk.`{:.codelink}]({{ site.baseurl }}{%link codici/linregrsk.py %}) Regressione lineare su scikit-learn (script)
 1. [`linregr.`{:.codelink}]({{ site.baseurl }}{%link codici/linregr.py %}) Visualizzazione andamento errore in regressione (script)
-1. [`linregr1.`{:.codelink}]({{ site.baseurl }}{%link codici/linregr1.py %}) Regressione lineare con gradiente (script)
+1. [`linregr_gradient.`{:.codelink}]({{ site.baseurl }}{%link codici/linregr_gradient.py %}) Regressione lineare con gradiente (notebook)
 1. [`BetaBernoulli.`{:.codelink}]({{ site.baseurl }}{%link codici/BetaBernoulli.ipynb %}) Esempio di inferenza bayesiana di probabilità p di una Bernoulli (notebook)
 1. [`ModelSelection.`{:.codelink}]({{ site.baseurl }}{%link codici/model_selection.ipynb %}) Esempio di model selection con test set e con information criteria (notebook)
 1. [`NaiveBayes.`{:.codelink}]({{ site.baseurl }}{%link codici/naivebayes.ipynb %}) Esempio di classificatore naive bayes con modelli di linguaggio e selezione termini più significativi (notebook)
--- codici/linregr1.py
+++ codici/linregr1.py
@@ -6,13 +6,27 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-import seaborn as sns
+
+plt.style.use('fivethirtyeight')
+
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['font.serif'] = 'Ubuntu'
+plt.rcParams['font.monospace'] = 'Ubuntu Mono'
+plt.rcParams['font.size'] = 10
+plt.rcParams['axes.labelsize'] = 10
+plt.rcParams['axes.labelweight'] = 'bold'
+plt.rcParams['axes.titlesize'] = 10
+plt.rcParams['xtick.labelsize'] = 8
+plt.rcParams['ytick.labelsize'] = 8
+plt.rcParams['legend.fontsize'] = 10
+plt.rcParams['figure.titlesize'] = 12
+plt.rcParams['image.cmap'] = 'jet'
+plt.rcParams['image.interpolation'] = 'none'
+plt.rcParams['figure.figsize'] = (12, 6)
+plt.rcParams['lines.linewidth'] = 2
 
 # definisce un vettore di colori
-colors = sns.color_palette("husl", 6)
-# dichiara alcune proprietà grafiche della figura
-sns.set(style="darkgrid", context='paper', palette=colors, rc={"figure.figsize": (16, 8),'image.cmap': 'jet', 'lines.linewidth':1})
-
+colors = ['#008fd5', '#fc4f30', '#e5ae38', '#6d904f', '#8b8b8b', '#810f7c', '#137e6d', '#be0119', '#3b638c', '#af6f09']
 
 def normalizza(X):
     mu = np.mean(X, axis=0)
@@ -26,12 +40,11 @@ data = pd.read_csv("mammals.csv", delimiter=',')
 n = len(data)
 
 # visualizza dati mediante scatter
-fig = plt.figure()
-fig.patch.set_facecolor('white')
+fig = plt.figure(figsize=(16,8))
 ax = fig.gca()
 ax.scatter(data['body'], data['brain'], s=20,c='r', marker='o', alpha=.5)
-plt.xlabel(u'Peso corporeo', fontsize=14)
-plt.ylabel('Peso del cervello', fontsize=14)
+plt.xlabel(u'Peso corporeo', fontsize=10)
+plt.ylabel('Peso del cervello', fontsize=10)
 plt.show()
 
 # Regressione mediante discesa del gradiente on-line, una dimensione alla volta
@@ -59,20 +72,18 @@ def error(v1, v2):
 v_error=np.vectorize(error)
 e=v_error(w0,w1).T
 
-fig = plt.figure()
-fig.patch.set_facecolor('white')
+fig = plt.figure(figsize=(16,8))
 ax = fig.gca(projection='3d')
-ax.plot_surface(w0, w1, e, rstride=1, cstride=1, cmap=plt.cm.jet , linewidth=0, antialiased=False)
+ax.plot_surface(w0, w1, e, rstride=1, cstride=1, cmap=plt.cm.jet , linewidth=0, antialiased=True)
 ax.tick_params(axis='x', labelsize=8)
 ax.tick_params(axis='y', labelsize=8)
 ax.tick_params(axis='z', labelsize=8)
-plt.xlabel(r"$\theta_0$", fontsize=12)
-plt.ylabel(r"$\theta_1$", fontsize=12)
+plt.xlabel(r"$\theta_0$", fontsize=10)
+plt.ylabel(r"$\theta_1$", fontsize=10)
 plt.title(r"Errore quadratico medio al variare dei coefficienti $w_0,w_1$", fontsize=12)
 plt.show()
 
-fig = plt.figure()
-fig.patch.set_facecolor('white')
+fig = plt.figure(figsize=(16,8))
 ax = fig.gca(projection='3d')
 ax.contour(w0, w1, e, np.logspace(3.5,4.2,400), colors=colors, linewidth=1)
 ax.tick_params(axis='x', labelsize=8)
@@ -83,11 +94,10 @@ plt.ylabel(r"$w_1$", fontsize=12)
 plt.title(r"Errore quadratico medio al variare dei coefficienti $w_0,w_1$", fontsize=12)
 plt.show()
 
-fig = plt.figure(figsize=(12,12))
-fig.patch.set_facecolor('white')
+fig = plt.figure(figsize=(16,8))
 ax = fig.gca()
 im = plt.imshow(e, origin='lower', extent=(w0_list.min(),w0_list.max(),w1_list.min(), w1_list.max()), aspect='auto',alpha=.8)
-plt.contour(w0, w1, e,color='r', lw=0.7)
+#plt.contour(w0, w1, e,color='r', lw=0.7)
 ax.tick_params(axis='x', labelsize=8)
 ax.tick_params(axis='y', labelsize=8)
 plt.xlabel(r"$w_0$", fontsize=12)
@@ -118,18 +128,18 @@ alphas = [0.01, 0.03, 0.1, 0.3, 0.5, 1]
 
 number_of_iterations = range(1, iterations + 1)
 
-fig = plt.figure()
-fig.patch.set_facecolor('white')
+fig = plt.figure(figsize=(16,8))
 for c,alpha in enumerate(alphas):
     costs, w = gradiente(X, t, w_init, alpha, iterations, n)
     ax = fig.add_subplot(3, 2, c+1, axisbg="#F8F8F8")
     ax = fig.gca()
-    ax.plot(number_of_iterations, costs, c= colors[c], alpha=.5, lw=1.5)
+    ax.plot(number_of_iterations, costs, c= colors[c], alpha=.8, lw=2, label =r'$\alpha={0:3.2f}$'.format(alpha) )
     plt.xticks(fontsize=8)
     plt.yticks(fontsize=8)
     plt.xlabel(r'Numero di iterazioni', fontsize=8)
     plt.ylabel(r'Costo', fontsize=8)
-    plt.title(r'$\alpha$='+str(alpha)+r': $c$='+str(round(costs[-1],3))+r'; $w_0$='+str(round(w[0][0],2))+'; $w_1$='+str(round(w[1][0],2))+'', fontsize=10)
+    plt.legend(fontsize=8)
+    plt.title(r'$c={1:6.2f}; w_0={2:8.2f}; w_1={2:8.2f}$'.format(round(costs[-1],3), round(w[0][0],2), round(w[1][0],2)), fontsize=8)
 plt.tight_layout()
 plt.show()
 
